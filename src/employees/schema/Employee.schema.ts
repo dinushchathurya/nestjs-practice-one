db64378247b5b2e96c68f567db0f9cbd4345a602
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-18 20:44:56 +0530

    :sparkles: Create schema for Employee

---
+++ src/employees/schema/Employee.schema.ts
@@ -0,0 +1,12 @@
+import { EmployeeTier, EmployeeStatus } from "../Employee.enum";
+
+/* Mongoose way */
+// export const EmployeeSchema = ({
+//     id: String,
+//     firstName: String,
+//     lastName: String,
+//     designation: String,
+//     nearestCity: String,
+//     tier: EmployeeTier,
+//     status: EmployeeStatus
+// });
