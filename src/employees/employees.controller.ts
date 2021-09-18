00109f424441d058b75ce575bbb987bc5c859442
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-18 09:44:37 +0530

    :sparkles: Create employee.service.ts & peering it in employees.controller.ts

--- src/employees/employees.controller.ts
+++ src/employees/employees.controller.ts
@@ -1,6 +1,11 @@
 import { Controller } from '@nestjs/common';
+import { EmployeesService } from './employees.service';
 
 @Controller('employees')
 export class EmployeesController {
+
+    constructor(private employeeService:EmployeesService) {
+
+    }
     
 }
