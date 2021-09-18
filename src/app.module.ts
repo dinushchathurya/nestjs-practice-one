f9b5f26eea5f0888f722a07e967cfa82b70f1879
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-18 20:15:49 +0530

    :sparkles: Connect application to MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { EmployeesModule } from './employees/employees.module';
+import { MONGO_URI } from './app.propeties';
 
 @Module({
-  imports: [EmployeesModule],
+  imports: [
+    EmployeesModule, 
+    MongooseModule.forRoot(MONGO_URI)
+  ]
 })
 export class AppModule {}
