3ba5bd874ae6627c4f69558f816e61033fead494
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-18 12:56:51 +0530

    :hammer: Implement moved employeeSearch in employees.service.ts

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Employee, EmployeeTier, EmployeeStatus } from './employee.model';
 import { v1 as uuid } from 'uuid';
+import { EmployeeSearchDto } from './employeeSearch.dto';
 
 @Injectable()
 export class EmployeesService {
@@ -24,5 +25,21 @@ export class EmployeesService {
         }
         this.employees.push(employee);
         return employee;
+    }
+
+    employeeSearch(employeeSearchDto: EmployeeSearchDto) {
+
+        const { status, name } = employeeSearchDto;
+        let employees = this.getAllEmployees();
+
+        if (status) {
+            employees = employees.filter(employee => employee.status === status);
+        }
+
+        if (name) {
+            employees = employees.filter(employee => employee.firstName.includes(name) || employee.lastName.includes(name));
+        }
+
+        return employees;
     }
 }
